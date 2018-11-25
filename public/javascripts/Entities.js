b2836d14b68b61e946197564f9f0bba3b2231395
DaNya <deadlyshine@gmail.com>
2018-11-25 15:02:56 +0300

    add Entities prototypes of main objects

--- public/javascripts/Entities.js
+++ public/javascripts/Entities.js
@@ -4,9 +4,174 @@ class Entity
     pos_y = 0;
     size_x = 0;
     size_y = 0;
+
+    constructor(pos_x, pos_y, size_x, size_y)
+    {
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.size_x = size_x;
+        this.size_y = size_y;
+        // empty for while
+    }
 }
 
 class Player extends Entity
 {
+    lifetime = 0;
+    move_x = 0;
+    move_y = 0;
+    speed = 1;
 
+    constructor(lifetime, pos_x, pos_y, size_x, size_y)
+    {
+        super(pos_x, pos_y, size_x, size_y);
+        this.lifetime = lifetime;
+    }
+
+    draw(ctx)
+    {
+        // draw object
+    }
+
+    update()
+    {
+        // update in cycle
+    }
+
+    onTouchEntity(obj)
+    {
+        // collide entities handle
+    }
+
+    kill()
+    {
+        // destroy this
+    }
+
+    fire()
+    {
+        // attack
+        let rocketSize_x = 32;
+        let rocketSize_y = 32;
+        let rocketMove_x = this.move_x;
+        let rocketMove_y = this.move_y;
+        let rocketName = "rocket" + (++gameManager.fireNum);
+        let rocket = new Rocket(rocketName,rocketMove_x,rocketMove_y,0,0,rocketSize_x,rocketSize_y);
+        switch(this.move_x + 2*this.move_y)
+        {
+            case -1:
+                rocket.pos_x = this.pos_x-rocket.size_x;
+                rocket.pos_y = this.pos_y;
+                break;
+            case 1:
+                rocket.pos_x = this.pos_x+this.size_x;
+                rocket.pos_y = this.pos_y;
+                break;
+            case -2:
+                rocket.pos_x = this.pos_x;
+                rocket.pos_y = this.pos_y-rocket.size_y;
+                break;
+            case 2:
+                rocket.pos_x = this.pos_x;
+                rocket.pos_y = this.pos_y+this.size_y;
+                break;
+            default:
+                return;
+        }
+        gameManager.entities.push(rocket);
+
+    }
+}
+
+class Tank extends Entity
+{
+    lifetime = 0;
+    move_x = 0;
+    move_y = -1;
+    speed = 1;
+
+    constructor(lifetime, pos_x, pos_y, size_x, size_y)
+    {
+        super(pos_x, pos_y, size_x, size_y);
+        this.lifetime = lifetime;
+    }
+
+    draw(ctx)
+    {
+        // draw object
+    }
+
+    update()
+    {
+        // update in cycle
+    }
+
+    onTouchEntity(obj)
+    {
+        // collide entities handle
+    }
+
+    kill()
+    {
+        // destroy this
+    }
+
+    fire()
+    {
+        // attack
+    }
+}
+
+class Rocket extends Entity
+{
+    name = "";
+    move_x = 0;
+    move_y = 0;
+    speed = 4;
+
+    constructor(name, move_x, move_y, pos_x, pos_y, size_x, size_y)
+    {
+        super(pos_x, pos_y, size_x, size_y);
+        this.name = name;
+        this.move_x = move_x;
+        this.move_y = move_y;
+    }
+
+    draw(ctx)
+    {
+        // draw object
+    }
+
+    update()
+    {
+        // update in cycle
+    }
+
+    onTouchEntity(obj)
+    {
+        // collide entities handle
+    }
+
+    onTouchMap(idx)
+    {
+        // collide map handle
+    }
+
+    kill()
+    {
+        // destroy this
+    }
+}
+
+class Bonus extends Entity
+{
+    draw(ctx)
+    {
+        // draw object
+    }
+
+    kill()
+    {
+        // destroy this
+    }
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,6 @@ let ctx = canvas.getContext("2d");
 
 mapManager = new MapManager('/levels/level1test.json');
 mapManager.draw(ctx);
+
+let hello = new Player(100);
+console.log(hello);
